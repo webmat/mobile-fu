a298314fc8a257a24933a1d2036856825556485a
Mathieu Martin <webmat@gmail.com>
2011-05-27 16:13:54 -0400

    Add tests for method I wanna tweak

--- spec/mobile_fu_spec.rb
+++ spec/mobile_fu_spec.rb
@@ -4,7 +4,7 @@ describe ActionController::MobileFu do
   before(:each) do
     @view = mock(:ActionView)
     @request = mock(:ActionRequest)
-    @view.extend(MobilizedStyles)
+    @view.extend(ActionController::MobileFu::InstanceMethods)
     @view.stub!(:request).and_return(@request)
     @request.stub!(:user_agent)
   end
@@ -13,7 +13,10 @@ describe ActionController::MobileFu do
     @request.stub!(:user_agent).and_return(str)
   end
 
-  it "will not explode" do
-    ''.should == ''
+  it "should let us detect user agents by providing a string" do
+    ua 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16'
+    @view.is_device?('iphone').should be_true
+    @view.is_device?('iPhone').should be_true
+    @view.is_device?('iPad').should   be_false
   end
 end
